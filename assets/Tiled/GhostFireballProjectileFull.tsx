ff89e9691683af478869b0f99e61741cf2b55d0d
sammybernau <sambernau04@gmail.com>
2024-08-15 17:42:29 -0500

    Added tsx files

---
+++ assets/Tiled/GhostFireballProjectileFull.tsx
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="GhostFireballProjectileFull" tilewidth="11" tileheight="23" tilecount="3" columns="3">
+ <image source="../textures/samsRendition/humanoid/GhostFireballProjectileFull.png" width="33" height="23"/>
+ <tile id="0" type="ghost_fireball_f1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="1" width="9" height="21">
+    <properties>
+     <property name="BodyType" value="dynamic"/>
+    </properties>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1" type="ghost_fireball_f2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="1" width="9" height="21">
+    <properties>
+     <property name="BodyType" value="dynamic"/>
+    </properties>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2" type="ghost_fireball_f3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="1" width="9" height="21">
+    <properties>
+     <property name="BodyType" value="dynamic"/>
+    </properties>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
---
+++ assets/Tiled/GhostFireballProjectileSmall.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="GhostFireballProjectileSmall" tilewidth="16" tileheight="16" tilecount="1" columns="1">
+ <image source="../textures/samsRendition/humanoid/GhostFireball_projectile_small.png" width="16" height="16"/>
+ <tile id="0" type="ghost_fireball_small">
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="ghost_fireball_small" x="4" y="0" width="9" height="16">
+    <properties>
+     <property name="BodyType" value="dynamic"/>
+    </properties>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
